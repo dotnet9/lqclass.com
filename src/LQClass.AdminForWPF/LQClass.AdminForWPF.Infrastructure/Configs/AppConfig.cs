195833de3159f6c6913c03df0c5ed93dba33a6bb
dotnet9 <632871194@qq.com>
2020-12-15 23:24:41 +0800

    添加WPF管理端，调试登录API接口

---
+++ src/LQClass.AdminForWPF/LQClass.AdminForWPF.Infrastructure/Configs/AppConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LQClass.AdminForWPF.Infrastructure.Configs
+{
+    /// <summary>
+    /// 对应config.json文件
+    /// </summary>
+    public class AppConfig
+    {
+        /// <summary>
+        /// 开发环境配置
+        /// </summary>
+        public ConfigDesc Development { get; set; }
+
+        /// <summary>
+        /// 生产环境配置
+        /// </summary>
+        public ConfigDesc Production { get; set; }
+    }
+
+    /// <summary>
+    /// 配置详细信息
+    /// </summary>
+    public class ConfigDesc
+    {
+        /// <summary>
+        /// api地址
+        /// </summary>
+        public string API { get; set; }
+    }
+}
---
+++ src/LQClass.AdminForWPF/LQClass.AdminForWPF/Views/LoginView.xaml.cs
@@ -0,0 +1,63 @@
+﻿using LQClass.AdminForWPF.Infrastructure.Configs;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace LQClass.AdminForWPF.Views
+{
+    /// <summary>
+    /// LoginView.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var configFile = $"{AppDomain.CurrentDomain.BaseDirectory}config.json";
+                var configContent = File.ReadAllText(configFile);
+                AppConfig appConfig = JsonConvert.DeserializeObject<AppConfig>(configContent);
+                string apiAddress = string.Empty;
+#if DEBUG
+                apiAddress = appConfig.Development.API;
+#else
+                apiAddress = appConfig.Production.API;
+#endif
+                var response = Login(apiAddress, tbName.Text, tbPwd.Password);
+                if (response.IsSuccessful)
+                {
+                    MessageBox.Show($"登录成功：{response.Content}");
+                }
+                else
+                {
+                    MessageBox.Show($"登录失败：{response.Content}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private IRestResponse Login(string apiAddress, string userName, string pwd)
+        {
+            var client = new RestClient($"{apiAddress}_login/Login");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AlwaysMultipartFormData = true;
+            request.AddParameter("userid", userName);
+            request.AddParameter("password", pwd);
+            request.AddParameter("cookie", "false");
+            IRestResponse response = client.Execute(request);
+
+            return response;
+        }
+    }
+}
